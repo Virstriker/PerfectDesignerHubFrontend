0d905f8af722fb0a816217ad0e2241f217554781
vinayak  gohel <104017167+Virstriker@users.noreply.github.com>
2024-11-10 14:10:42 +0530

    123

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class constant {
-  public Url: string = 'http://192.168.1.9:5035/';
-  public pdfUrl: string = 'http://localhost:3000/'
-  // public Url: string = 'https://perfectdesignerhub.runasp.net/';
-  // public pdfUrl: string = 'https://pdf-generator-node-js.vercel.app/'
+  // public Url: string = 'http://192.168.1.9:5035/';
+  // public pdfUrl: string = 'http://localhost:3000/'
+  public Url: string = 'https://perfectdesignerhub.runasp.net/';
+  public pdfUrl: string = 'https://pdf-generator-node-js.vercel.app/'
 }
