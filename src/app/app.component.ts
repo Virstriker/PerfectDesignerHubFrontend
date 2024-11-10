6c22861f823e3eb7f7cdf298a9aa33be4dad1d6c
vinayak  gohel <104017167+Virstriker@users.noreply.github.com>
2024-11-10 14:10:26 +0530

    13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,36 +8,36 @@ import { Router, RouterModule, RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements  OnInit {
-  constructor(private route:Router){}
+export class AppComponent{
+  // constructor(private route:Router){}
   title = 'perfect';
-  ngOnInit(): void {
-    const token = localStorage.getItem('token');
-    if (token) {
-      const decodedToken = this.decodeToken(token);
-      const currentTime = Math.floor(Date.now() / 1000);
+  // ngOnInit(): void {
+  //   const token = localStorage.getItem('token');
+  //   if (token) {
+  //     const decodedToken = this.decodeToken(token);
+  //     const currentTime = Math.floor(Date.now() / 1000);
 
-      if (decodedToken.exp < currentTime) {
-        localStorage.removeItem('token');
-        this.route.navigateByUrl('/login');
-      }
-    } else {
-      this.route.navigateByUrl('/login');
-    }
-  }
+  //     if (decodedToken.exp < currentTime) {
+  //       localStorage.removeItem('token');
+  //       this.route.navigateByUrl('/login');
+  //     }
+  //   } else {
+  //     this.route.navigateByUrl('/login');
+  //   }
+  // }
 
-  private decodeToken(token: string): any {
-    try {
-      const base64Url = token.split('.')[1];
-      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-      const jsonPayload = decodeURIComponent(atob(base64).split('').map((c) => {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-      }).join(''));
+  // private decodeToken(token: string): any {
+  //   try {
+  //     const base64Url = token.split('.')[1];
+  //     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  //     const jsonPayload = decodeURIComponent(atob(base64).split('').map((c) => {
+  //       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  //     }).join(''));
 
-      return JSON.parse(jsonPayload);
-    } catch (error) {
-      console.error('Invalid token', error);
-      return null;
-    }
-  }
+  //     return JSON.parse(jsonPayload);
+  //   } catch (error) {
+  //     console.error('Invalid token', error);
+  //     return null;
+  //   }
+  // }
 }
